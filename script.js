924c3ef92e37f7dfa542ca67dc0f9c4e670cfeab
BetterChoise <147253188+BetterChoise@users.noreply.github.com>
2023-11-29 19:11:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// let menu = document.querySelector('#menu-icon');
+//         let navbar = document.querySelector('.navbar');
+
+//         menu.onclick = () =>{   
+//             menu.classList.toggle('bx-x');
+//             navbar.classList.toggle('open');
+//         };
+
+//         $('.navbar a').click(function(){
+//             $('.navbar a').removeClass('active');
+//             $(this).addClass('active');   
+//         });
